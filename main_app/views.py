545fcff1d370f1970b9fdcbdae06da6fc0c42622
Nathan Daniels <nathandanielsbassoon@gmail.com>
2020-08-10 16:45:31 -0500

    Small tweaks

--- main_app/views.py
+++ main_app/views.py
@@ -71,7 +71,7 @@ def update(request):
             profile = form.save()
             return redirect('profile')
         else:
-            return redirect('home')
+            return redirect('update')
     else:
         form = ProfileForm(instance=profile)
         return render(request, 'registration/update.html', {'form': form, 'profile': profile})
